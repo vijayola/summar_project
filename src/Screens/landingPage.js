0a3ada4906507e6e99bd931b7ba7af1f3493adbc
kamal7643 <kamalswami0808@gmail.com>
2021-04-11 11:31:18 +0530

    hello world

---
+++ src/Screens/landingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <header className="LandingPage-header">
+        <p>
+          Hello world!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default LandingPage;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import LandingPage from './Screens/landingPage';
+import About from './Screens/about';
+import Signup from './Screens/signUp';
+import Login from './Screens/login';
+import Profile from './Screens/profile';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Router>
+        <switch>
+          <Route path="/" exact component={LandingPage}></Route>
+        </switch>
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
